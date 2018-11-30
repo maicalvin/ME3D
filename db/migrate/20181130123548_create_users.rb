bdf5e4c90519cd9c20a9efe60a95d0714509781e
Mai Calvin <mai.calvin@yahoo.com>
2018-11-30 20:53:08 +0800

    Added User Migration file

---
+++ db/migrate/20181130123548_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string  :first_name,null: false
+      t.string  :last_name,null: false
+      t.string :gender, null: false
+      t.string :email, null: false
+      t.date :birthday,null:false
+      t.string :password_digestß
+      t.timestamps
+    end
+  end
+end

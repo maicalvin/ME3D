67794fd5b1e8df5669d3b3a284688ab93d6d22ef
Mai Calvin <mai.calvin@yahoo.com>
2018-12-07 00:07:38 +0800

    Capybara test

--- spec/view/signin.html.erb_spec.rb
+++ spec/view/signin.html.erb_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 RSpec.feature "signin", type: :feature do
-    context "Display the email and password login form" do 
+    context "User Sign In" do 
         scenario 'should be successful' do
-                fill_in 'user_email', :with=>'maicalvin@yahoo.com'
-            within('password_field') do
-                fill_in 'password', with: '1234567'
-            end
-        click_button('submit')
-        expect(page).to have_content('Welcome to ME3D')
+            visit sign_in_path
+            expect(page).to have_text("Sign In")
+            fill_in 'email', :with=>'maicalvin@yahoo.com'
+            fill_in 'password', with: '123456'
+            click_button('Sign In')
+        end
+    end
+    context "User Sign Up" do 
+        scenario 'should be successful' do
+            visit root_url
+            expect(page).to have_content("Home Sign Up Sign In MAKE ME3D Find your reliable 3D printing service provider in nearby. ")
+            fill_in 'First Name', :with=>'mai'
+            fill_in 'Last Name', :with=>'calvin'
+            fill_in 'Email', :with=>'mai@yahoo.com'
+            fill_in 'password', :with=>'123456'
+            fill_in 'yyyy-mm-dd', with: '1995-12-11'
+            click_button('SIGN UP')
         end
     end
 end
+
